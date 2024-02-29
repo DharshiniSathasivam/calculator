2909011c94aac2c44a8c35c896d763d3c8c56f1c
DharshiniSathasivam <dharshinisatha54@gmail.com>
2024-02-29 11:49:07 -0500

    Calculator

---
+++ script.js
@@ -0,0 +1,18 @@
+const NewDisplay =document.getElementById("result")
+
+function appendToDisplay(input){
+    NewDisplay.value += input
+}
+
+function calculate(){
+    try{
+        NewDisplay.value = eval(NewDisplay.value)
+    }
+    catch(error){
+        NewDisplay.value = "Error"
+    }
+}
+
+function clearDisplay(){
+    NewDisplay.value =""
+}
